7bfd53afbc3c2c8a11e631c0807a88922ab732ab
Ramiro <ramirorojtberg@gmail.com>
2022-10-04 18:47:41 -0300

    Ejercicios 4-5-6

---
+++ Ej-4-5-6/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Clock from './components/lifeCycleClass';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Clock></Clock>
+      </header>
+    </div>
+  );
+}
+
+export default App;
